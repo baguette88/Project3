81e170c6ef8eb3f9a426036fc92f72065c953a11
jbrown7407 <jbrown7407@gmail.com>
2020-11-16 14:38:36 -0500

    ability to add products

---
+++ products_api/controllers/products.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const products = express.Router();
+
+const Product = require("../models/products.js");
+
+// index
+products.get("/", (req, res) => {
+  Product.find({}, (error, foundProducts) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    res.status(200).json(foundProducts);
+  });
+});
+
+// create
+products.post("/", (req, res) => {
+  Product.create(req.body, (error, createdProduct) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    res.status(200).json(createdProduct);
+  });
+});
+
+// delete
+products.delete("/:id", (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (error, deletedProduct) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    res.status(200).json(deletedProduct);
+  });
+});
+
+// update
+products.put("/:id", (req, res) => {
+  Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (error, updatedProduct) => {
+      if (error) {
+        res.status(400).json({ error: error.message });
+      }
+      res.status(200).json(updatedProduct);
+    }
+  );
+});
+
+module.exports = products;
