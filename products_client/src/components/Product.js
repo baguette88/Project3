c7d9cae77f4397cf669a734d6626d2cbf0cbfb55
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2020-11-17 10:53:11 -0500

    made product and hero template

---
+++ products_client/src/components/Product.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class ProductBox extends Component {
+  render() {
+    return <div className="ProductBox">
+      <h1>Product Name</h1>
+
+      <img  />
+      <h2> Price</h2>
+      <p>Product descripton header</p>
+      <p>Product description</p>
+    </div>;
+
+  }
+}
