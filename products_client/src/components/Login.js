1b5b18e742f74b1f949ce64064e5e8fcc33be0c3
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2020-11-17 21:09:53 -0500

    fixed errors

--- products_client/src/components/Login.js
+++ products_client/src/components/Login.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export default class Login extends Component {
   render() {
     return <div className="ProductBox">
-      <h1>Welcom To Emazon</h1>
+      <h1>Welcom To E-mazon</h1>
         <form>
-            <label>Username: <input class="input"></input></label> <br>
-            <label>Password: <input class="input"></input></label> <br>
+            <label>Username: <input class="input"></input></label> <br />
+            <label>Password: <input class="input"></input></label> <br />
             <button className="btn-login" type="submit">Login</button>
 
         </form>
