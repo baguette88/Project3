56443ff067ea8fa45b9f32050c83e170a33e37a9
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-11-18 11:24:50 -0500

    added inputs to NewForm component

--- products_api/models/products.js
+++ products_api/models/products.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
+  price: { type: Number, default: "100" },
   image: { type: String },
-  description: { type: String, default: "Best Product ever!" },
-  tags: [{ type: String }],
   category: { type: String, default: "Category!" },
   subcategory: { type: String, default: "SubCategory!" },
-  price: { type: Number, default: "100" },
-  reviews: [{ type: String }],
+  brand: { type: String, required: true },
+  description: { type: String, default: "Best Product ever!" },
   inStock: { type: Boolean, default: true },
-  quantity: [{type: Number, default: "0" }]
+  quantity: [{type: Number, default: 0, min: [0, 'Out of Stock'] }],
+  tags: [{ type: String }], 
+  reviews: [{ type: String }]  
 });
 
 module.exports = mongoose.model("Product", productSchema);
