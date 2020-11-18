9e13e48ac9c8e757cb57bf6f9bbeebbef37bd8ad
masterkong06 <wintonbw@gmail.com>
2020-11-18 10:11:32 -0500

    ProductCart component completed and renders on page

--- products_client/src/components/ProductGrid.js
+++ products_client/src/components/ProductGrid.js
@@ -6,7 +6,7 @@ export default class ProductGrid extends Component {
             <div id="product-grid">
             <h1>PRODUCT GRID RENDERS</h1>
             {/* wrap this in some loop that produces additional div*/}
-            <img src="https://media.gq-magazine.co.uk/photos/5dbc4d5a8da8f900083b9076/master/w_1000,c_limit/20191028-watch-guide-baume.jpg" alt='' />
+            <img src="https://media.gq-magazine.co.uk/photos/5dbc4d5a8da8f900083b9076/master/w_1000,c_limit/20191028-watch-guide-baume.jpg" width="185px" alt='' />
             <button className="view-product-btn">View</button>{/* button positioned at higher z-index and positioned over image*/}
                <ul>
                     <li>Product Brand</li>
