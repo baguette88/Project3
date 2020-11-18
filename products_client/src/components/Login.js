bf54fb23109d8bb207c8826e09909ca2e03cab5e
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2020-11-17 21:00:26 -0500

    fixed spelling error

--- products_client/src/components/Login.js
+++ products_client/src/components/Login.js
@@ -5,9 +5,9 @@ export default class Login extends Component {
     return <div className="ProductBox">
       <h1>Welcom To Emazon</h1>
         <form>
-            <lebal>Username: <input class="input"></input></lebal>
-            <lebal>Password: <input class="input"></input></lebal>
-            <button className="btn login" type="submit">Login</button>
+            <label>Username: <input class="input"></input></label>
+            <label>Password: <input class="input"></input></label>
+            <button className="btn-login" type="submit">Login</button>
 
         </form>
     </div>;
