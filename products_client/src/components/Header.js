736d8fd3e36c31525f1058f66bf2d95d9b95bb56
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2020-11-20 16:11:10 -0500

    added a few new ccs properties to header

--- products_client/src/components/Header.js
+++ products_client/src/components/Header.js
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 // import '../index.css'
+
 
 
 export default class Header extends Component {
   render() {
     return (
+
+      
+
       <div className="navbarTop">
         <div className="ScreenTop"> </div>
         <div className="Banner">
           {" "}
-          E-Mazon <br />
+          <Jumbotron>
+            <h1 className="muted">E-Mazon</h1>
+           <br />
           <span ClassName="logo">Logo</span>
           <i className="fa fa-shopping-cart"></i>
+          <div className="navbar">
+       
+       <ListGroup horizontal className="list">
+         
+         <ListGroup.Item>Gift Cards</ListGroup.Item>
+         <ListGroup.Item> Get Ultra</ListGroup.Item>
+         <ListGroup.Item>Find a Gift</ListGroup.Item>
+         <ListGroup.Item>Special Sales</ListGroup.Item>
+         <ListGroup.Item>We are Hiring </ListGroup.Item>
+       </ListGroup>
+      
+      
+     </div>
+          </Jumbotron>
         </div>
         <div className="SearchBar">
           <form onSubmit={this.handleSubmit}>
@@ -20,16 +42,9 @@ export default class Header extends Component {
             <i type="submit" className="fa fa-search"></i>
           </form>
         </div>
-        <div className="navbar">
-          {" "}
-          <ul>
-            {" "}
-            <li> Holiday Deals </li> |<li> Gift Cards </li> |
-            <li> Get Ultra </li> |<li> Find a Gift </li> |
-            <li> Special Sales </li> |<li> We are Hiring </li>
-          </ul>{" "}
-        </div>
+       
       </div>
+    
     );
   }
 }
