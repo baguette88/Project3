1396b32fc8496c1ac79060b3fad7cb6d516cf216
masterkong06 <wintonbw@gmail.com>
2020-11-20 16:33:38 -0500

    ProductCart css work progress: buttons styled and item and list are aligned side-by-side.

--- products_client/src/components/ProductCart.js
+++ products_client/src/components/ProductCart.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import InputGroup from 'react-bootstrap/InputGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Col from 'react-bootstrap/ListGroup';
 
 export default class ProductCart extends Component {
     render() {
@@ -14,21 +15,26 @@ export default class ProductCart extends Component {
                     <div className="subtotal">
                         <p>Subtotal (# of items): $0.00</p>
                     </div>
-                <Button className="checkout-btn">Checkout Emazon Cart</Button>
-                <hr />
-                <div id="products-in-cart">
-                    <img src="https://media.gq-magazine.co.uk/photos/5dbc4d5a8da8f900083b9076/master/w_1000,c_limit/20191028-watch-guide-baume.jpg" width="110px" className="cart-product-img" alt=""/>
-                    <div className="cart-product-details">
-                        <ListGroup>
-                            <ListGroup.Item>Product Name</ListGroup.Item>
-                            <ListGroup.Item>$99.99</ListGroup.Item>
-                            <ListGroup.Item>In Stock</ListGroup.Item>
-                           <ListGroup.Item>Style:</ListGroup.Item>
-                           <ListGroup.Item>Color:</ListGroup.Item>
-                        </ListGroup>
-                    </div>
-                    <div className="items-in-cart"></div>
-                </div>
+                    <Button className="checkout-btn">Checkout Emazon Cart</Button>
+                    <hr className="item_hr"/>
+                    <Row>
+                    {/* <div id="products-in-cart"> */}
+                       <Col > <img src="https://media.gq-magazine.co.uk/photos/5dbc4d5a8da8f900083b9076/master/w_1000,c_limit/20191028-watch-guide-baume.jpg" width="110px" className="cart-product-img" alt=""/>
+                       </Col>
+                        {/* <div className="cart-product-details"> */}
+                            <Col>
+                                <ListGroup className="test">
+                                    <ListGroup.Item className="product_name">Baume 10601</ListGroup.Item>
+                                    <ListGroup.Item className="product_price">$99.99</ListGroup.Item>
+                                    <ListGroup.Item className="product_in_stock">In Stock</ListGroup.Item>
+                                    <ListGroup.Item className="product_style">Style:</ListGroup.Item>
+                                    <ListGroup.Item className="product_color">Color:</ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                        {/* </div> */}
+                        {/* <div className="items-in-cart"><p>aasd</p></div> */}
+                    {/* </div> */}
+                </Row>
                 <div className="items-in-cart">
                     <div className="cart-update">
                         <form action="" id="cart-qty">
@@ -38,8 +44,8 @@ export default class ProductCart extends Component {
                                     <InputGroup id="quantity" name="quantity" type="text" className="qty-in-cart"></InputGroup>
                                     <Button className="increase-qty-btn">Add Item</Button>
 
-                                        <Button className="delete-item-btn">Delete</Button>
-                                        <Button className="save-item-btn">Save for Later</Button>
+                                        <Button variant="secondary" className="delete-item-btn">Delete</Button>
+                                        <Button variant="secondary" className="save-item-btn">Save for Later</Button>
                                 </Row>
                             </Container>
                         </form>
